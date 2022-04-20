aa2958ecdf08205c1cb53c2a79d0f70832dc9fa9
CrypticZero(Tom) <spikezor322@gmail.com>
2022-04-20 10:50:36 +0200

    Update Fundamentals.php

--- Fundamentals.php
+++ Fundamentals.php
@@ -142,6 +142,16 @@
     echo "<br>";
 
     // Assignment 2: Write a PHP script which will display the colors in the following way
-    
+    $color = array("white", "green", "red");
+    echo "<ul>" .
+            "<li>" . $color[1] . "</li>" .
+            "<li>" . $color[2] . "</li>" .
+            "<li>" . $color[0] . "</li>" .
+         "</ul>";
 
+    echo "<br>";
+
+    //Assignment 3: Create a PHP script which displays the capital and country name from the above array $ceu. Sort the list by the capital of the country.
+    $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+    
 ?>
